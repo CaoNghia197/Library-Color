4b984ef1d5f278e55477e3c5692ed2213144bce9
nghia cau <caonghia97@gmail.com>
2022-01-25 14:09:42 +0700

    Initial commit

---
+++ app/src/main/java/com/sample/library_color/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sample.library_color;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
